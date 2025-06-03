3e329525d5123ef8401902a63b6e6e1bc8953c8f
Just Fontaine <itsmealeexvii@gmail.com>
2025-06-03 18:21:33 -0300

    feat: modulo color-thief declarado

---
+++ src/color-thief.d.ts
@@ -0,0 +1,6 @@
+declare module 'color-thief' {
+  export default class ColorThief {
+    getColor(img: HTMLImageElement | HTMLCanvasElement): [number, number, number];
+    getPalette(img: HTMLImageElement | HTMLCanvasElement, colorCount?: number): [number, number, number][];
+  }
+}
