59b21ccd6bc8313ed33bd4000c652236c53e2177
Just Fontaine <itsmealeexvii@gmail.com>
2025-06-18 19:02:33 -0300

    chore: manejo de estado guardado/guardar en biblioteca para canciones

--- src/app/components/song-card/song-card.component.ts
+++ src/app/components/song-card/song-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import type { Song } from '../../models/song.model';
+import { LibraryService } from '../../services/library.service';
 
 @Component({
   selector: 'app-song-card',
@@ -11,13 +12,34 @@ import type { Song } from '../../models/song.model';
 })
 export class SongCardComponent {
   @Input() song!: Song;
+  @Input() showSaveButton: boolean = false;
   @Output() selected = new EventEmitter<Song>();
+  @Output() save = new EventEmitter<Song>()
+
+  savedSongs: Song[] = [];
+
+  constructor (private libraryService: LibraryService) {}
 
   get cardTypeClass(): string {
     return this.song.type || 'album-track'; 
   }
 
+  ngOnInit(): void{
+    this.libraryService.getSavedSongs().subscribe(savedSongs => {
+      this.savedSongs = savedSongs;
+    });
+  }
+
   onSelect(): void {
     this.selected.emit(this.song);
+  }
+
+  onSave(event: Event): void{
+    event.stopPropagation(); 
+    this.save.emit(this.song);
+  }
+
+  isSongSaved(): boolean{
+    return this.savedSongs.some(savedSong => savedSong.id === this.song.id);
   }
 }
